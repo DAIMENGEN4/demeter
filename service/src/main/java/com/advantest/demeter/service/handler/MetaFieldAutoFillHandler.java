a585da7198b141fc711c2f713c6eeb2d6bd185ae
mengen.dai <mengen.dai@outlook.com>
2025-02-12 21:50:06 +0800

    refactor: 调整 MetaFieldAutoFillHandler 包名将 MetaFieldAutoFillHandler 类从 database 包移动到 service 包，以更好地反映其作为服务层处理器的职责。这有助于改进项目结构的清晰度和可维护性。

--- service/src/main/java/com/advantest/demeter/service/handler/MetaFieldAutoFillHandler.java
+++ service/src/main/java/com/advantest/demeter/service/handler/MetaFieldAutoFillHandler.java
@@ -1,4 +1,4 @@
-package com.advantest.demeter.database.handler;
+package com.advantest.demeter.service.handler;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import org.apache.ibatis.reflection.MetaObject;
