e2f42b66270114583dca34955f43949c863d04c2
mengen.dai <mengen.dai@advantest.com>
2025-02-13 17:44:25 +0800

    refactor(database): 移除多个实体类中的不必要的 Lombok 注解
    - 在 DepartmentPO、HolidayPO、ProjectPO、ProjectTaskAttributeBooleanValuePO、ProjectTaskAttributeDatetimeValuePO、ProjectTaskAttributeDateValuePO、ProjectTaskAttributeDoubleValuePO、ProjectTaskAttributeFloatValuePO、ProjectTaskAttributeIntegerValuePO、ProjectTaskAttributeJsonValuePO、ProjectTaskAttributeLongtextValuePO、ProjectTaskAttributeLongValuePO、ProjectTaskAttributeMediumtextValuePO、ProjectTaskAttributePO、ProjectTaskAttributeStringValuePO、ProjectTaskAttributeTextValuePO、ProjectTaskPO 和 TeamPO 类中移除了 @NoArgsConstructor 注解
    - 此次重构减少了代码冗余，简化了实体类的结构

--- database/src/main/java/com/advantest/demeter/database/po/ProjectTaskAttributeLongtextValuePO.java
+++ database/src/main/java/com/advantest/demeter/database/po/ProjectTaskAttributeLongtextValuePO.java
@@ -6,7 +6,6 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
