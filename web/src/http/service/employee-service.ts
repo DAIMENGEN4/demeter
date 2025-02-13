345ae745ceeace0ab21339c8c911d63ced4c61da
mengen.dai <mengen.dai@advantest.com>
2025-02-13 14:57:30 +0800

    feat(web): 添加员工服务模块 - 新增 EmployeeService 类，实现员工注册功能 - 定义 EmployeeDTO 接口，用于描述员工数据结构 - 使用单例模式管理 EmployeeService 实例 - 通过 HttpClient 发送注册请求，并处理成功和失败回调

---
+++ web/src/http/service/employee-service.ts
@@ -0,0 +1,27 @@
+import {HttpClient} from "@D/http/http-client.ts";
+
+export interface EmployeeDTO {
+    readonly id: string;
+    readonly account: string;
+    readonly password: string;
+    readonly employeeName: string;
+    readonly email: string;
+    readonly isActive: boolean;
+}
+
+export class EmployeeService {
+
+    private static instance: EmployeeService;
+
+    public static getInstance(): EmployeeService {
+        if (!EmployeeService.instance) {
+            EmployeeService.instance = new EmployeeService();
+        }
+        return EmployeeService.instance;
+    }
+
+    public registerRequest(params: EmployeeDTO, success: (response: boolean) => void, failure?: (error: Error) => void) {
+        const url = "/v1/employee/register";
+        HttpClient.post<boolean>(url, {...params}).then(success).catch(failure);
+    }
+}
