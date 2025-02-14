f1af602a15e4ffaf35fd9b9c88963c96fd200921
mengen.dai <mengen.dai@advantest.com>
2025-02-14 12:55:28 +0800

    refactor(web): 重构项目视图对象
    - 将 ProjectVo 类从 use-project-vo.tsx 文件中移出，创建独立的 project-vo.ts 文件
    - 更新 use-project-vo.tsx 文件，使用新的 ProjectVo 类
    -此重构旨在提高代码的可维护性和可重用性，将视图对象与钩子函数解耦

---
+++ web/src/views/vo/project-vo.ts
@@ -0,0 +1,59 @@
+import {ProjectDTO} from "@D/http/service/project-service.ts";
+
+export class ProjectVo {
+    private readonly id: number;
+    private readonly projectName: string;
+    private readonly description: string;
+    private readonly startDateTime: string;
+    private readonly endDateTime: string;
+    private readonly projectStatus: number;
+    private readonly version: number;
+    private readonly order: number;
+
+    constructor(id: number, projectName: string, description: string, startDateTime: string, endDateTime: string, projectStatus: number, version: number, order: number) {
+        this.id = id;
+        this.projectName = projectName;
+        this.description = description;
+        this.startDateTime = startDateTime;
+        this.endDateTime = endDateTime;
+        this.projectStatus = projectStatus;
+        this.version = version;
+        this.order = order;
+    }
+
+    getId(): number {
+        return this.id;
+    }
+
+    getProjectName(): string {
+        return this.projectName;
+    }
+
+    getDescription(): string {
+        return this.description;
+    }
+
+    getStartDateTime(): string {
+        return this.startDateTime;
+    }
+
+    getEndDateTime(): string {
+        return this.endDateTime;
+    }
+
+    getProjectStatus(): number {
+        return this.projectStatus;
+    }
+
+    getVersion(): number {
+        return this.version;
+    }
+
+    getOrder(): number {
+        return this.order;
+    }
+
+    static of(dto: ProjectDTO): ProjectVo {
+        return new ProjectVo(dto.id, dto.projectName, dto.description, dto.startDateTime, dto.endDateTime, dto.projectStatus, dto.version, dto.order);
+    }
+}
