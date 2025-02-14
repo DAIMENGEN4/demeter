f341d2fa99c0a731335a1830f5cbeafd263bbc6c
mengen.dai <mengen.dai@advantest.com>
2025-02-14 10:11:49 +0800

    feat(web): 新增项目服务接口和相关方法
    - 添加 ProjectDTO 接口定义项目相关属性
    - 实现 ProjectService 类，包含以下功能：
      - 创建项目
      - 删除项目
      - 更新项目
      - 获取当前员工的项目列表
    - 使用单例模式管理 ProjectService 实例
    - 通过 HttpClient 发送 HTTP 请求实现上述功能

---
+++ web/src/http/service/project-service.ts
@@ -0,0 +1,47 @@
+import {HttpClient} from "@D/http/http-client.ts";
+
+export interface ProjectDTO {
+    id: number;
+    projectName: string;
+    description: string;
+    startDateTime: string;
+    endDateTime: string;
+    projectStatus: number;
+    version: number;
+    order: number;
+}
+
+
+export class ProjectService {
+
+    private static instance: ProjectService;
+
+    public static getInstance(): ProjectService {
+        if (!ProjectService.instance) {
+            ProjectService.instance = new ProjectService();
+        }
+        return ProjectService.instance;
+    }
+
+    public createProjectRequest(params: ProjectDTO, success: (response: ProjectDTO) => void, failure?: (error: Error) => void) {
+        const url = "/v1/project/createProject";
+        HttpClient.post<ProjectDTO>(url, {...params}).then(success).catch(failure);
+    }
+
+    public deleteProjectRequest(params: {
+        projectId: string
+    }, success: (response: boolean) => void, failure?: (error: Error) => void) {
+        const url = "/v1/project/deleteProject";
+        HttpClient.post<boolean>(url, {...params}).then(success).catch(failure);
+    }
+
+    public updateProjectRequest(params: ProjectDTO, success: (response: ProjectDTO) => void, failure?: (error: Error) => void) {
+        const url = "/v1/project/updateProject";
+        HttpClient.post<ProjectDTO>(url, {...params}).then(success).catch(failure);
+    }
+
+    public getProjectsByCurrentEmployeeRequest(success: (response: Array<ProjectDTO>) => void, failure?: (error: Error) => void) {
+        const url = "/v1/project/getProjectsByCurrentEmployee";
+        HttpClient.post<ProjectDTO[]>(url).then(success).catch(failure);
+    }
+}
