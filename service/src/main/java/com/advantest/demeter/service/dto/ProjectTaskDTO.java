6fd98e70efb64007fbce1efa8127f3880f7e778a
mengen.dai <mengen.dai@advantest.com>
2025-02-19 16:52:47 +0800

    refactor(service): 重构 ProjectTaskDTO 类
    - 添加多个任务属性相关字段，支持不同数据类型的属性值
    - 使用 record 关键字简化类定义
    - 引入 Lombok 的 @Builder 注解
    - 移除静态工厂方法 of

--- service/src/main/java/com/advantest/demeter/service/dto/ProjectTaskDTO.java
+++ service/src/main/java/com/advantest/demeter/service/dto/ProjectTaskDTO.java
@@ -1,23 +1,33 @@
 package com.advantest.demeter.service.dto;
 
-import com.advantest.demeter.database.po.ProjectTaskPO;
+import com.fasterxml.jackson.databind.JsonNode;
+import lombok.Builder;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * Create on 2025/01/01
  * Author: mengen.dai@outlook.com
  */
+@Builder
 public record ProjectTaskDTO(
         Long id,
         String taskName,
         Long parentTaskId,
-        Long projectId
+        List<ProjectTaskAttributeDTO> taskAttributes,
+        List<ProjectTaskAttributeValueDTO<Long>> taskAttributeLongValues,
+        List<ProjectTaskAttributeValueDTO<Boolean>> taskAttributeBooleanValues,
+        List<ProjectTaskAttributeValueDTO<Double>> taskAttributeDoubleValues,
+        List<ProjectTaskAttributeValueDTO<Integer>> taskAttributeIntegerValues,
+        List<ProjectTaskAttributeValueDTO<Float>> taskAttributeFloatValues,
+        List<ProjectTaskAttributeValueDTO<String>> taskAttributeTextValues,
+        List<ProjectTaskAttributeValueDTO<String>> taskAttributeVarcharValues,
+        List<ProjectTaskAttributeValueDTO<String>> taskAttributeLongTextValues,
+        List<ProjectTaskAttributeValueDTO<String>> taskAttributeMediumtextTextValues,
+        List<ProjectTaskAttributeValueDTO<JsonNode>> taskAttributeJsonValues,
+        List<ProjectTaskAttributeValueDTO<LocalDate>> taskAttributeDateValues,
+        List<ProjectTaskAttributeValueDTO<LocalDateTime>> taskAttributeDateTimeValues
 ) {
-    public static ProjectTaskDTO of(ProjectTaskPO projectTaskPO) {
-        return new ProjectTaskDTO(
-                projectTaskPO.getId(),
-                projectTaskPO.getTaskName(),
-                projectTaskPO.getParentTaskId(),
-                projectTaskPO.getProjectId()
-        );
-    }
 }
